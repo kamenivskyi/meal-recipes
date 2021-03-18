69a75eba1a5b4eba958921a580168c910c5b8747
Roman Kamenivskyi <rk@wwwest.solutions>
2021-03-18 21:15:56 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FoodList />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FoodList from "@/components/FoodList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    FoodList,
   }
 }
 </script>
---
+++ src/components/FoodList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Food list here
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FoodList',
+  mounted() {
+    console.log('Mounted')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
